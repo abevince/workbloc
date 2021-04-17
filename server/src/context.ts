cbc80a738a561308b628bcef645d81765c8b149d
Vincent Abesamis <vincentabesamis03@gmail.com>
2021-04-17 09:11:02 +0800

    Add authorization logic

--- server/src/context.ts
+++ server/src/context.ts
@@ -1,19 +1,89 @@
 import { PrismaClient } from "@prisma/client";
+import { FastifyReply, FastifyRequest } from "fastify";
+import Redis, { Redis as RedisT } from "ioredis";
+import { SessionObjectType } from "./utils/createSession";
+
+const redis = new Redis({
+  port: 6379,
+  host: "127.0.0.1",
+});
 
 const prisma = new PrismaClient({
   log: ["query", "info", "warn"],
 });
 
-export interface Context {
-  prisma: PrismaClient;
+interface UserAuthDataType {
+  userId: string;
+  role: "ADMIN" | "USER";
 }
 
-export const context: Context = {
+interface AuthType {
+  isAuthenticated: boolean;
+  user: UserAuthDataType | null;
+}
+
+export interface Context {
+  prisma: PrismaClient;
+  request: FastifyRequest;
+  reply: FastifyReply;
+  redis: RedisT;
+  auth: AuthType;
+}
+
+export const context: Partial<Context> = {
   prisma: prisma,
 };
 
-export const buildContext = async (): Promise<Context> => {
-  return {
+export const buildContext = async (
+  req: FastifyRequest,
+  reply: FastifyReply
+): Promise<Context> => {
+  const ctx: Context = {
     prisma: prisma,
+    request: req,
+    reply: reply,
+    redis: redis,
+    auth: { isAuthenticated: false, user: null },
   };
+  try {
+    if (!req.cookies[process.env.COOKIE_NAME || "sessid"]) {
+      return ctx;
+    }
+
+    const { value, valid } = req.unsignCookie(
+      req.cookies[process.env.COOKIE_NAME || "sessid"]
+    );
+    if (!value || valid === false) {
+      return ctx;
+    }
+
+    const session = await redis.get(value);
+    if (!session) {
+      return ctx;
+    }
+
+    const sessionData = JSON.parse(session) as SessionObjectType;
+    if (!sessionData.isValid) {
+      return ctx;
+    }
+
+    const foundUser = await prisma.user.findUnique({
+      where: { id: sessionData.userId },
+    });
+
+    if (!foundUser) {
+      return ctx;
+    }
+
+    return {
+      ...ctx,
+      auth: {
+        isAuthenticated: true,
+        user: { userId: foundUser.id, role: foundUser.role },
+      },
+    };
+  } catch (error) {
+    console.log("Context", error);
+    throw Error("Something went wrong");
+  }
 };
