6f1afe63b6d46db887266f2a32c6e9ac98713429
Vincent Abesamis <vincentabesamis03@gmail.com>
2021-04-23 11:00:04 +0800

    Add ProfileResolver

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import "reflect-metadata";
 import Fastify, { FastifyInstance } from "fastify";
 import mercurius from "mercurius";
+import { buildSchema } from "type-graphql";
 import fastifyCors from "fastify-cors";
 import sensible from "fastify-sensible";
 import cookie, { FastifyCookieOptions } from "fastify-cookie";
 
 import { UserResolver } from "./resolvers/User";
-import { buildSchema } from "type-graphql";
+import { ProfileResolver } from "./resolvers/Profile";
 
 import { buildContext } from "./context";
 
@@ -22,7 +23,7 @@ async function main() {
   } as FastifyCookieOptions);
 
   const schema = await buildSchema({
-    resolvers: [UserResolver],
+    resolvers: [UserResolver, ProfileResolver],
     validate: false,
   });
 
