4cde77b14f679ee6932dd170526c03e312eb91c9
Vincent Abesamis <vincentabesamis03@gmail.com>
2021-04-25 19:04:13 +0800

    Create worklog item schema

---
+++ server/src/schema/WorklogItems.schema.ts
@@ -0,0 +1,23 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import { Worklog } from "./Worklog.schema";
+
+@ObjectType()
+export class WorklogItem {
+  @Field(() => Int, { nullable: false })
+  readonly id: number;
+
+  @Field(() => Worklog)
+  worklog?: Worklog;
+
+  @Field(() => Int, { nullable: false })
+  worklogId!: number;
+
+  @Field(() => Date, { nullable: false })
+  timeFrom!: Date;
+
+  @Field(() => Date, { nullable: false })
+  timeTo!: Date;
+
+  @Field(() => String, { nullable: false })
+  workDone!: string;
+}
