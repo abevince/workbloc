6ef97993322cf7b1896c116dc1e3f09853497237
Vincent Abesamis <vincentabesamis03@gmail.com>
2021-04-27 15:34:47 +0800

    Add worklog items and user field resolvers

--- server/src/resolvers/Worklog/index.ts
+++ server/src/resolvers/Worklog/index.ts
@@ -1,11 +1,52 @@
-import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  FieldResolver,
+  Mutation,
+  Query,
+  Resolver,
+  Root,
+} from "type-graphql";
 import { Context } from "../../context";
+import { User } from "../../schema/User.schema";
 import { Worklog } from "../../schema/Worklog.schema";
+import { WorklogItem } from "../../schema/WorklogItems.schema";
 import { isToday } from "../../utils/isToday";
 import { WorklogUniqueInput } from "./inputs";
 
 @Resolver(Worklog)
 export class WorklogResolver {
+  @FieldResolver(() => User, { nullable: true })
+  user(
+    @Root() worklog: Worklog,
+    @Ctx() { prisma }: Context
+  ): Promise<User | null> {
+    return prisma.user.findUnique({
+      where: { id: worklog.userId },
+      select: {
+        id: true,
+        email: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+        status: true,
+        suspensionReason: true,
+      },
+    });
+  }
+
+  @FieldResolver(() => WorklogItem, { nullable: true })
+  worklogItem(
+    @Root() worklog: Worklog,
+    @Ctx() { prisma }: Context
+  ): Promise<WorklogItem[] | null> {
+    return prisma.worklog
+      .findUnique({
+        where: { id: worklog.id },
+      })
+      .WorklogItem({});
+  }
+
   @Query(() => Worklog, { nullable: true })
   async worklog(
     @Arg("where") { id }: WorklogUniqueInput,
@@ -16,8 +57,12 @@ export class WorklogResolver {
     });
   }
   // TODO: Get worklog/s. With dynamic filtering and pagination
-  // @Query()
-  // worklogs() {}
+  @Query(() => [Worklog])
+  async worklogs(@Ctx() { prisma }: Context): Promise<Worklog[] | null> {
+    const worklogs = prisma.worklog.findMany();
+    if (!worklogs) return null;
+    return worklogs;
+  }
 
   @Mutation(() => Boolean)
   async createWorklog(@Ctx() { auth, prisma }: Context): Promise<boolean> {
